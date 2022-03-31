1be9609d6ce4d4bda976168bf38d0e7e9f924a9e
navduhan <duhan27dec@gmail.com>
2022-03-31 12:47:06 -0600

    update

--- src/pages/Sequences/fetchSequence.jsx
+++ src/pages/Sequences/fetchSequence.jsx
@@ -4,7 +4,7 @@ export const fetchSequence = async (mdata, flank, filename )=>{
 
   for (let data of mdata) {
     data['flank'] = flank
-    const res = await axios.get(`http://localhost:3603/api/seq?chr=${data.chromosome}&start=${data.motif_start-flank}&stop=${data.motif_end+flank}&filename=${filename}`);
+    const res = await axios.get(`http://bioinfo.usu.edu/ranchbackend/api/seq?chr=${data.chromosome}&start=${data.motif_start-flank}&stop=${data.motif_end+flank}&filename=${filename}`);
 
     const fasta = res.data;
     
